e145ed2cbce80ed8521c9e8ce9b1062538076d06
tcklpl <negroniluan@gmail.com>
2022-02-23 13:14:18 -0300

    data formats: vec2-4 and mat3-4

---
+++ visualizer/src/scripts/engine/data_formats/i_uniformable.ts
@@ -0,0 +1,6 @@
+
+export interface IUniformable {
+
+    bindUniform(gl: WebGL2RenderingContext, to: WebGLUniformLocation): void;
+
+}
---
+++ visualizer/src/scripts/engine/data_formats/vec/vec2.ts
@@ -0,0 +1,60 @@
+import { MUtils } from "../../utils/math_utils";
+import { IUniformable } from "../i_uniformable";
+
+export class Vec2 implements IUniformable {
+
+    protected _values: number[];
+
+    constructor(x: number, y: number) {
+        this._values = [x, y];
+    }
+
+    bindUniform(gl: WebGL2RenderingContext, to: WebGLUniformLocation): void {
+        gl.uniform2fv(to, new Float32Array(this.values));
+    }
+
+    sum(x: number, y: number) {
+        x += x;
+        y += y;
+    }
+
+    addVec2(other: Vec2) {
+        this.sum(other.x, other.y);
+    }
+
+    differentFrom(other: Vec2) {
+        return this.x != other.x || this.y != other.y;
+    }
+
+    public get values() {
+        return this._values;
+    }
+
+    public get x() {
+        return this._values[0];
+    }
+
+    public set x(v: number) {
+        this._values[0] = v;
+    }
+
+    public get y() {
+        return this._values[1];
+    }
+
+    public set y(v: number) {
+        this._values[1] = v;
+    }
+
+    static clamp(min: Vec2, max: Vec2, value: Vec2) {
+        return new Vec2(
+            MUtils.clamp(min.x, max.x, value.x),
+            MUtils.clamp(min.y, max.y, value.y)
+        );
+    }
+
+    static fromValue(val: number) {
+        return new Vec2(val, val);
+    }
+
+}
