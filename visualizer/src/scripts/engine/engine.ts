8bdae71039f414fbf694a060a23ecd159990726a
tcklpl <negroniluan@gmail.com>
2022-03-18 13:13:28 -0300

    frame listeners

--- visualizer/src/scripts/engine/engine.ts
+++ visualizer/src/scripts/engine/engine.ts
@@ -1,6 +1,7 @@
 import { Visualizer } from "../visualizer/visualizer";
 import { Renderer } from "./renderer";
 import { Time } from "./time";
+import { IFrameListener } from "./traits/i_frame_listener";
 
 export class Engine {
 
@@ -8,11 +9,15 @@ export class Engine {
 
     private _lastFrame: number = 0;
     private _gl = Visualizer.instance.gl;
+
+    private _frameListeners: IFrameListener[] = [];
     
     render(time: number) {
         const msDiff = time - this._lastFrame;
         Time.deltaTime = msDiff / 1000;
         this._lastFrame = time;
+
+        this._frameListeners.forEach(f => f.update());
 
         this._renderer.renderActive();
 
@@ -23,6 +28,10 @@ export class Engine {
         this._gl.canvas.width = this._gl.canvas.clientWidth;
         this._gl.canvas.height = this._gl.canvas.clientHeight;
         this._renderer.resize(this._gl.canvas.width, this._gl.canvas.height);
+    }
+
+    registerFrameListener(l: IFrameListener) {
+        this._frameListeners.push(l);
     }
 
     public get renderer() {
---
+++ visualizer/src/scripts/engine/traits/i_frame_listener.ts
@@ -0,0 +1,6 @@
+
+export interface IFrameListener {
+
+    update(): void;
+
+}
