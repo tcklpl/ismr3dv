a51332a93350225ede01308b05190cdb877b7290
tcklpl <negroniluan@gmail.com>
2022-04-03 14:45:16 -0300

    added an enviroment file to the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import compression from 'compression';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
-const port = 3333;
+const port = process.env.PORT;
+
+if (!port) {
+    console.error("Failed to acquire the server listening port from the enviroment file. Is the .env file at your current directory?");
+    process.exit(1);
+}
 
 server.use(compression());
 server.use(express.static(path.join(__dirname, "..", "..", "visualizer", "out")));
--- setup.sh
+++ setup.sh
@@ -19,6 +19,27 @@ then
     exit
 fi
 
+if ! command -v sed > /dev/null
+then
+    echo "[!] sed was not found, please install it or correct your path."
+    exit
+fi
+
+SERVER_PORT=3333
+
+echo -n "[?] What port should the server run in? (default is 3333): "
+read desired_server_port
+if ! [[ -z "$desired_server_port" ]]; then
+    SERVER_PORT=$desired_server_port
+fi
+
+if [ $SERVER_PORT -gt 65535 ] || [ $SERVER_PORT -lt 1 ]; then
+    echo "[!] Please input a valid port number (1 - 65535)"
+    exit
+fi
+
+sed "s/<PORT>/$SERVER_PORT/g" .env.model > .env
+
 echo "[-] Setting up server..."
 cd server
 npm install
