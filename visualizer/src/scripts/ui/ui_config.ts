7c611aab3e73ace6eca45f1acf2eb55ca0a3d99b
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-04-10 13:51:05 -0300

    storage monitor and more icons

--- visualizer/src/scripts/ui/ui_config.ts
+++ visualizer/src/scripts/ui/ui_config.ts
@@ -1,5 +1,7 @@
 import { IGeneralConfiguration } from "../engine/config/general_configuration";
 import { IGraphicalConfiguration } from "../engine/config/graphical_configuration";
+import { StorageType } from "../local_storage/storage_type";
+import { SizeNameUtils } from "../visualizer/utils/size_name_utils";
 import { Visualizer } from "../visualizer/visualizer";
 import { UIInfo } from "./ui_info";
 
@@ -13,6 +15,7 @@ export class UIConfig {
         $('#cfg-graphical-btn').on('click', () => {
             this.setupGeneral(configManager.general);
             this.setupGraphical(configManager.graphical);
+            this.setupStorage();
         });
 
         $('#cfg-btn-save').on('click', () => {
@@ -53,6 +56,31 @@ export class UIConfig {
         graphicalConfig.bloom = $('#cfg-bloom').is(':checked');
         graphicalConfig.earth_texture_size = $('#ctf-earth-texsize').val() as string;
         graphicalConfig.sun_texture_size = $('#ctf-sun-texsize').val() as string;
+    }
+
+    private static setupStorage() {
+        const storage = Visualizer.instance.storageController;
+
+        const totalSize = storage.maxSizeKb * 1000;
+
+        const configSize = storage.storagePerCategory.get(StorageType.CONFIG) ?? 0;
+        const cacheSize = storage.storagePerCategory.get(StorageType.CACHE) ?? 0;
+
+        $('#cfg-storage-info-total-space').html(`Total: ${SizeNameUtils.getNameBySize(totalSize, ['B', 'KB', 'MB'], 1000)}`);
+        $('#cfg-storage-info-used-space').html(`In use: ${SizeNameUtils.getNameBySize(storage.totalUsedSizeBytes, ['B', 'KB', 'MB'], 1000)}`);
+
+        $('#cfg-storage-info-config-size').html(`${SizeNameUtils.getNameBySize(configSize, ['B', 'KB', 'MB'], 1000)}`);
+        $('#cfg-storage-info-cache-size').html(`${SizeNameUtils.getNameBySize(cacheSize, ['B', 'KB', 'MB'], 1000)}`);
+        $('#cfg-storage-info-free-size').html(`${SizeNameUtils.getNameBySize(storage.totalFreeSpaceBytes, ['B', 'KB', 'MB'], 1000)}`);
+
+        $('#cfg-storage-pb-config').css('width', `${configSize / totalSize * 100}%`);
+        $('#cfg-storage-pb-cache').css('width', `${cacheSize / totalSize * 100}%`);
+
+        $('#btn-clear-storage-cache').prop('disabled', cacheSize <= 0 ? 'disabled': '');
+        $('#btn-clear-storage-cache').on('click', () => {
+            storage.removeCategory(StorageType.CACHE);
+            this.setupStorage();
+        });
     }
 
     private static checkForTextureSizeSupport(elementValue: string) {
