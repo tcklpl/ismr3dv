49976eef52dce29307908bda2bcb11a19eda8372
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-04-11 13:21:34 -0300

    changed request to use params instead of body

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,6 +3,6 @@ import station_list from "./endpoints/station_list";
 
 const routes = Router();
 
-routes.get("/stations", station_list.fetchStationList);
+routes.get("/stations/:startdate/:enddate", station_list.fetchStationList);
 
 export default routes;
