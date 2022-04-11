aa596f4a7d53d2a105dea617e531740adf423eb4
tcklpl <negroniluan@gmail.com>
2022-04-11 13:22:01 -0300

    confirmation dialogue

---
+++ visualizer/src/scripts/ui/confirmation_screen.ts
@@ -0,0 +1,17 @@
+type bootstrap = (...args: any[]) => any;
+
+export class ConfirmationScreen {
+
+    constructor(title: string, description: string, onConfirm: () => void, optionYes?: string, optionNo?: string) {
+        const optYes = optionYes ?? "Yes";
+        const optNo = optionNo ?? "No";
+
+        $('#confirmation-modal-title').html(title);
+        $('#confirmation-modal-body').html(description);
+        $('#confirmation-modal-no').html(optNo);
+        $('#confirmation-modal-yes').html(optYes).off('click').on('click', () => onConfirm());
+
+        $('#confirmation-modal-show-btn').trigger('click');
+    }
+
+}
