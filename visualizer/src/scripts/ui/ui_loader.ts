f15ffcc31f58220f23948c0e5eff7c22993fc094
tcklpl <negroniluan@gmail.com>
2022-04-13 11:51:48 -0300

    connection failed screen and more icons

--- visualizer/src/scripts/ui/ui_loader.ts
+++ visualizer/src/scripts/ui/ui_loader.ts
@@ -1,8 +1,10 @@
+import { SizeNameUtils } from "../visualizer/utils/size_name_utils";
 
 export class UILoader {
 
     updateStatus(percentage: number, message: string) {
         $('#loading-progress-bar').attr('aria-valuenow', percentage).css('width', `${percentage}%`);
+        $('#loading-message-percentage').html(`${SizeNameUtils.truncateNumber(percentage, 1)}%`);
         $('#loading-message').html(message);
     }
 
--- visualizer/src/scripts/visualizer/utils/size_name_utils.ts
+++ visualizer/src/scripts/visualizer/utils/size_name_utils.ts
@@ -5,10 +5,14 @@ export class SizeNameUtils {
         let i = curIndex ?? 0;
         if (i > sizes.length) throw `Insufficient size name vector, trying to access index ${i} with value ${value}`;
         if (value < step) {
-            const isDecimal = Math.floor(value) != value;
-            return `${isDecimal ? Math.trunc(value * Math.pow(10, 2)) / Math.pow(10, 2) : value} ${sizes[i]}`;
+            return `${this.truncateNumber(value, 2)} ${sizes[i]}`;
         };
         return this.getNameBySize(value / step, sizes, step, i + 1);
+    }
+
+    static truncateNumber(value: number, decimal: number) {
+        const isDecimal = Math.floor(value) != value;
+        return isDecimal ? Math.trunc(value * Math.pow(10, decimal)) / Math.pow(10, decimal) : value;
     }
 
 }
