63fde9ecd73cc44a016589d864e747b899237997
tcklpl <negroniluan@gmail.com>
2022-04-13 23:06:41 -0300

    server force showcase mode and date fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ dotenv.config();
 const server = express();
 const envport = process.env.PORT;
 const envapikey = process.env.UNESP_ISMR_API_KEY;
+const envForceShowcase = process.env.FORCE_SHOWCASE_MODE;
 
 if (!envport) {
     console.error("[X] Failed to acquire the server listening port from the enviroment file. Is the .env file at your current directory?");
@@ -22,8 +23,9 @@ if (isNaN(port) || port < 1 || port > 65535) {
     process.exit(1);
 }
 
-new Config(envapikey ? envapikey : "");
+new Config(envapikey && (/false/i).test(`${envForceShowcase}`) ? envapikey : "");
 
+console.log(`[-] Server running in ${Config.instance.hasApiKey ? 'normal': 'showcase'} mode`);
 console.log("[-] Loading server");
 
 server.use(compression());
