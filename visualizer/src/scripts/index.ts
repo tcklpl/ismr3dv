02efb2736be96e5236895c8d33a1b2e5822fbca4
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-04-28 09:49:46 -0300

    changed main error handler to window.onerror

--- visualizer/src/scripts/index.ts
+++ visualizer/src/scripts/index.ts
@@ -1,23 +1,22 @@
 import { Visualizer } from "./visualizer/visualizer";
 
-try {
-    const canvas = $('#ismr3dcanvas');
-    const context = (canvas.get(0) as HTMLCanvasElement).getContext('webgl2');
-    if (!context) throw `Failed to acquire WebGL2 context`;
+const canvas = $('#ismr3dcanvas');
+const context = (canvas.get(0) as HTMLCanvasElement).getContext('webgl2');
+if (!context) throw `Failed to acquire WebGL2 context`;
 
-    const visualizer = new Visualizer(context);
-    canvas.on('click', e => {
-        document.getElementById('ismr3dcanvas')?.requestPointerLock();
-    });
-} catch (e) {
-    $('#crash-motive').html(e as string);
-    $('#error-screen').css('display', 'flex');
-    throw e;
-}
+const visualizer = new Visualizer(context);
+canvas.on('click', e => {
+    document.getElementById('ismr3dcanvas')?.requestPointerLock();
+});
 
 $(window).on("resize", () => {
     Visualizer.instance.engine.adjustToWindowSize();
 });
+
+window.onerror = (e, source) => {
+    $('#crash-motive').html(`'${e}' @ ${source}`);
+    $('#error-screen').css('display', 'flex');
+}
 
 document.addEventListener('pointerlockchange', e => {
     Visualizer.instance.pointerLocked = document.pointerLockElement === document.getElementById('ismr3dcanvas');
