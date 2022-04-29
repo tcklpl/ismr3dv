e7a096d95723f9e43c0dbc0a5d9db75758cf2724
tcklpl <negroniluan@gmail.com>
2022-04-29 19:32:57 -0300

    actual 2-pass gaussian blur

--- visualizer/src/scripts/engine/renderer/render_bloom_provider.ts
+++ visualizer/src/scripts/engine/renderer/render_bloom_provider.ts
@@ -7,6 +7,7 @@ import { TextureUtils } from "../utils/texture_utils";
 import { IRenderPostProcessingProvider } from "./i_prender_post_processing_provider";
 import { IRenderLayers } from "./i_render_layers";
 import { IRenderSettings } from "./i_render_settings";
+import { PingPongFramebuffer } from "./ping_pong_framebuffer";
 
 export class RenderBloomProvider implements IRenderPostProcessingProvider {
 
@@ -14,11 +15,14 @@ export class RenderBloomProvider implements IRenderPostProcessingProvider {
     private _shaderManager = Visualizer.instance.shaderManager;
 
     private _resolution!: Vec2;
+    private _altResolutions: Vec2[] = [];
+    private _aspectRatio!: number;
 
     private _gaussianShader!: Shader;
     private _gaussianUniformImage!: WebGLUniformLocation;
     private _gaussianUniformKernel!: WebGLUniformLocation[];
     private _gaussianUniformKernelSize!: WebGLUniformLocation;
+    private _gaussianUniformHorizontal!: WebGLUniformLocation;
 
     private _bloomResolutionPasses = [2, 3, 4];
     private _bloomBoxBlurIntensity = 1;
@@ -29,29 +33,31 @@ export class RenderBloomProvider implements IRenderPostProcessingProvider {
     private _bcPass3Uniform!: WebGLUniformLocation;
     private _bcBoxBlurIntensity!: WebGLUniformLocation;
 
-    private _gp1Framebuffer!: WebGLFramebuffer;
-    private _gp2Framebuffer!: WebGLFramebuffer;
-    private _gp3Framebuffer!: WebGLFramebuffer;
-
-    private _gp1Buffer!: WebGLTexture;
-    private _gp2Buffer!: WebGLTexture;
-    private _gp3Buffer!: WebGLTexture;
+    private _gp1Buffer!: PingPongFramebuffer;
+    private _gp2Buffer!: PingPongFramebuffer;
+    private _gp3Buffer!: PingPongFramebuffer;
 
     private _gpCombineBuffer!: WebGLTexture;
     private _gpCombineFramebuffer!: WebGLFramebuffer;
 
-    private _gKernel5 = MUtils.generateGaussianKernel(5, 10, 0, 0.3);
-    private _gKernel7 = MUtils.generateGaussianKernel(7, 14, 0, 0.3);
-    private _gKernel10 = MUtils.generateGaussianKernel(10, 20, 0, 0.3);
+    private _gKernel5 = MUtils.generateGaussianKernel(5, 10, 0, 0.5);
+    private _gKernel7 = MUtils.generateGaussianKernel(7, 14, 0, 0.5);
+    private _gKernel10 = MUtils.generateGaussianKernel(50, 100, 0, 0.1);
 
     setup(settings: IRenderSettings): void {
         this._resolution = new Vec2(settings.width, settings.height);
-        
-        this.setupBloomPassBuffers(settings.width, settings.height);
+        this.computeAltResolutions();
+
+        this.setupBloomCombineBuffer(settings.width, settings.height);
+
+        this._gp1Buffer = new PingPongFramebuffer(this._altResolutions[0]);
+        this._gp2Buffer = new PingPongFramebuffer(this._altResolutions[1]);
+        this._gp3Buffer = new PingPongFramebuffer(this._altResolutions[2]);
 
         this._gaussianShader = this._shaderManager.getByName('gaussian') as Shader;
         this._gaussianUniformImage = this._gaussianShader.assertGetUniform('u_image');
         this._gaussianUniformKernelSize = this._gaussianShader.assertGetUniform('u_kernel_size');
+        this._gaussianUniformHorizontal = this._gaussianShader.assertGetUniform('u_horizontal');
         this._gaussianUniformKernel = [];
         for (let i = 0; i < 10; i++) {
             this._gaussianUniformKernel.push(this._gaussianShader.assertGetUniform(`u_kernel[${i}]`));
@@ -64,37 +70,22 @@ export class RenderBloomProvider implements IRenderPostProcessingProvider {
         this._bcBoxBlurIntensity = this._bloomCombineShader.assertGetUniform('u_box_blur_intensity');
     }
 
-    private deleteBloomPassBuffers() {
-        this._gl.deleteFramebuffer(this._gp1Framebuffer);
-        this._gl.deleteFramebuffer(this._gp2Framebuffer);
-        this._gl.deleteFramebuffer(this._gp3Framebuffer);
+    private computeAltResolutions() {
+        this._altResolutions = [
+            new Vec2(this._resolution.x / this._bloomResolutionPasses[0], this._resolution.y / this._bloomResolutionPasses[0]),
+            new Vec2(this._resolution.x / this._bloomResolutionPasses[1], this._resolution.y / this._bloomResolutionPasses[1]),
+            new Vec2(this._resolution.x / this._bloomResolutionPasses[2], this._resolution.y / this._bloomResolutionPasses[2])
+        ];
+        this._aspectRatio = this._resolution.x / this._resolution.y;
+    }
+
+    private deleteBloomCombineBuffer() {
         this._gl.deleteFramebuffer(this._gpCombineFramebuffer);
-        this._gl.deleteTexture(this._gp1Buffer);
-        this._gl.deleteTexture(this._gp2Buffer);
-        this._gl.deleteTexture(this._gp3Buffer);
         this._gl.deleteTexture(this._gpCombineBuffer);
     }
 
-    private setupBloomPassBuffers(width: number, height: number) {
-        this.deleteBloomPassBuffers();
-
-        this._gp1Framebuffer = BufferUtils.createFramebuffer(this._gl);
-        this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, this._gp1Framebuffer);
-        this._gp1Buffer = TextureUtils.createBufferTexture(this._gl, width / this._bloomResolutionPasses[0], height / this._bloomResolutionPasses[0]);
-        this._gl.framebufferTexture2D(this._gl.FRAMEBUFFER, this._gl.COLOR_ATTACHMENT0, this._gl.TEXTURE_2D, this._gp1Buffer, 0);
-        BufferUtils.assertFrameBufferCompletion(this._gl, 'Failed to create bloom pass #1 framebuffer');
-
-        this._gp2Framebuffer = BufferUtils.createFramebuffer(this._gl);
-        this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, this._gp2Framebuffer);
-        this._gp2Buffer = TextureUtils.createBufferTexture(this._gl, width / this._bloomResolutionPasses[1], height / this._bloomResolutionPasses[1]);
-        this._gl.framebufferTexture2D(this._gl.FRAMEBUFFER, this._gl.COLOR_ATTACHMENT0, this._gl.TEXTURE_2D, this._gp2Buffer, 0);
-        BufferUtils.assertFrameBufferCompletion(this._gl, 'Failed to create bloom pass #2 framebuffer');
-
-        this._gp3Framebuffer = BufferUtils.createFramebuffer(this._gl);
-        this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, this._gp3Framebuffer);
-        this._gp3Buffer = TextureUtils.createBufferTexture(this._gl, width / this._bloomResolutionPasses[2], height / this._bloomResolutionPasses[2]);
-        this._gl.framebufferTexture2D(this._gl.FRAMEBUFFER, this._gl.COLOR_ATTACHMENT0, this._gl.TEXTURE_2D, this._gp3Buffer, 0);
-        BufferUtils.assertFrameBufferCompletion(this._gl, 'Failed to create bloom pass #3 framebuffer');
+    private setupBloomCombineBuffer(width: number, height: number) {
+        this.deleteBloomCombineBuffer();
 
         this._gpCombineFramebuffer = BufferUtils.createFramebuffer(this._gl);
         this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, this._gpCombineFramebuffer);
@@ -106,38 +97,49 @@ export class RenderBloomProvider implements IRenderPostProcessingProvider {
     }
 
     updateForResolution(width: number, height: number): void {
-        this.setupBloomPassBuffers(width, height);
         this._resolution.x = width;
         this._resolution.y = height;
+        this.computeAltResolutions();
+        this.setupBloomCombineBuffer(width, height);
+        this._gp1Buffer.changeResolution(this._altResolutions[0]);
+        this._gp2Buffer.changeResolution(this._altResolutions[1]);
+        this._gp3Buffer.changeResolution(this._altResolutions[2]);
     }
 
-    twoPassGaussianBlur(source: WebGLTexture, targetFb: WebGLFramebuffer, kernel: number[]) {
+    nPassGaussianBlur(source: WebGLTexture, pingPongFb: PingPongFramebuffer, kernel: number[], passes: number) {
         this._gaussianShader.bind();
-        this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, targetFb);
-        this._gl.uniform1i(this._gaussianUniformKernelSize, kernel.length);
         kernel.forEach((kv, i) => this._gl.uniform1f(this._gaussianUniformKernel[i], kv));
+        this._gl.uniform1i(this._gaussianUniformKernelSize, kernel.length);
         this._gl.uniform1i(this._gaussianUniformImage, 0);
-        this._gl.bindTexture(this._gl.TEXTURE_2D, source);
-        Visualizer.instance.engine.renderer.renderQuad();
+
+        for (let i = 0; i < passes; i++) {
+            const even = (i & 1) == 0;
+            this._gl.uniform1i(this._gaussianUniformHorizontal, i & 1);
+            this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, even ? pingPongFb.fbPing : pingPongFb.fbPong);
+            this._gl.bindTexture(this._gl.TEXTURE_2D, i == 0 ? source : even ? pingPongFb.texPong : pingPongFb.texPing);
+            Visualizer.instance.engine.renderer.renderQuad();
+        }
+
         this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, null);
+        return (passes & 1) == 1 ? pingPongFb.texPing : pingPongFb.texPong;
     }
 
     render(layers: IRenderLayers): void {
 
         // pass 1
-        this._gl.viewport(0, 0, this._resolution.x / this._bloomResolutionPasses[0], this._resolution.y / this._bloomResolutionPasses[0]);
+        this._gl.viewport(0, 0, this._altResolutions[0].x, this._altResolutions[0].y);
         this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
-        this.twoPassGaussianBlur(layers.raw_bloom, this._gp1Framebuffer, this._gKernel5);
+        const pass1 = this.nPassGaussianBlur(layers.raw_bloom, this._gp1Buffer, this._gKernel5, 2);
 
         // pass 2
-        this._gl.viewport(0, 0, this._resolution.x / this._bloomResolutionPasses[1], this._resolution.y / this._bloomResolutionPasses[1]);
+        this._gl.viewport(0, 0, this._altResolutions[1].x, this._altResolutions[1].y);
         this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
-        this.twoPassGaussianBlur(this._gp1Buffer, this._gp2Framebuffer, this._gKernel7);
+        const pass2 = this.nPassGaussianBlur(layers.raw_bloom, this._gp2Buffer, this._gKernel7, 2);
 
         // pass 3
-        this._gl.viewport(0, 0, this._resolution.x / this._bloomResolutionPasses[2], this._resolution.y / this._bloomResolutionPasses[2]);
+        this._gl.viewport(0, 0, this._altResolutions[2].x, this._altResolutions[2].y);
         this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
-        this.twoPassGaussianBlur(this._gp2Buffer, this._gp3Framebuffer, this._gKernel10);
+        const pass3 = this.nPassGaussianBlur(layers.raw_bloom, this._gp3Buffer, this._gKernel10, 3);
 
         // now combine all passes
         this._gl.viewport(0, 0, this._resolution.x, this._resolution.y);
@@ -148,15 +150,15 @@ export class RenderBloomProvider implements IRenderPostProcessingProvider {
 
         this._gl.uniform1i(this._bcPass1Uniform, 0);
         this._gl.activeTexture(this._gl.TEXTURE0);
-        this._gl.bindTexture(this._gl.TEXTURE_2D, this._gp1Buffer);
+        this._gl.bindTexture(this._gl.TEXTURE_2D, pass1);
 
         this._gl.uniform1i(this._bcPass2Uniform, 1);
         this._gl.activeTexture(this._gl.TEXTURE1);
-        this._gl.bindTexture(this._gl.TEXTURE_2D, this._gp2Buffer);
+        this._gl.bindTexture(this._gl.TEXTURE_2D, pass2);
 
         this._gl.uniform1i(this._bcPass3Uniform, 2);
         this._gl.activeTexture(this._gl.TEXTURE2);
-        this._gl.bindTexture(this._gl.TEXTURE_2D, this._gp3Buffer);
+        this._gl.bindTexture(this._gl.TEXTURE_2D, pass3);
 
         Visualizer.instance.engine.renderer.renderQuad();
         this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, null);
--- visualizer/src/shaders/gaussian_blur/gaussian_blur.frag
+++ visualizer/src/shaders/gaussian_blur/gaussian_blur.frag
@@ -8,27 +8,27 @@ in vec2 vtf_texCoords;
 out vec4 out_color;
 
 uniform sampler2D u_image;
-uniform float u_kernel[10];
+uniform float u_kernel[50];
 uniform int u_kernel_size;
+uniform bool u_horizontal;
 
 void main() {
     vec2 texOffset = vec2(1.0) / vec2(textureSize(u_image, 0));
-    vec3 result = texture(u_image, vtf_texCoords).rgb * u_kernel[0];
+    vec3 result = vec3(0.0);
 
-    // first blur horizontally
-    for (int i = 1; i < u_kernel_size; i++) {
-        float fi = float(i);
-        result += texture(u_image, vtf_texCoords + vec2(texOffset.x * fi, 0.0)).rgb * u_kernel[i];
-        result += texture(u_image, vtf_texCoords - vec2(texOffset.x * fi, 0.0)).rgb * u_kernel[i];
+    if (u_horizontal) {
+        for (int i = 0; i < u_kernel_size; i++) {
+            float fi = float(i);
+            result += texture(u_image, vtf_texCoords + vec2(texOffset.x * fi, 0.0)).rgb * u_kernel[i];
+            result += texture(u_image, vtf_texCoords - vec2(texOffset.x * fi, 0.0)).rgb * u_kernel[i];
+        }
+    } else {
+        for (int i = 0; i < u_kernel_size; i++) {
+            float fi = float(i);
+            result += texture(u_image, vtf_texCoords + vec2(0.0, texOffset.y * fi)).rgb * u_kernel[i];
+            result += texture(u_image, vtf_texCoords - vec2(0.0, texOffset.y * fi)).rgb * u_kernel[i];
+        }
     }
-
-    // then vertically
-    for (int i = 1; i < u_kernel_size; i++) {
-        float fi = float(i);
-        result += texture(u_image, vtf_texCoords + vec2(0.0, texOffset.x * fi)).rgb * u_kernel[i];
-        result += texture(u_image, vtf_texCoords - vec2(0.0, texOffset.x * fi)).rgb * u_kernel[i];
-    }
-
 
     out_color = vec4(result, 1.0);
 }
