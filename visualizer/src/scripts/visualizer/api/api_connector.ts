f22f0997e9165a68bdfbf245c0e287c67a4ba4a4
tcklpl <negroniluan@gmail.com>
2022-05-03 13:17:37 -0300

    removed unused code from the api connector

--- visualizer/src/scripts/visualizer/api/api_connector.ts
+++ visualizer/src/scripts/visualizer/api/api_connector.ts
@@ -8,12 +8,6 @@ export class ISMRAPIConnector {
 
     fetchStations(start: Date, end: Date) {
         return new Promise<IStationInfo[]>((resolve, reject) => {
-            
-            const request = <IStationInfoRequest>{
-                start_date: start,
-                end_date: end
-            };
-
             fetch(ISMRAPIEndpoints.STATION_LIST.replace(':startdate', start.toISOString()).replace(':enddate', end.toISOString()))
             .then(res => {
                 if (res.ok) {
@@ -30,7 +24,6 @@ export class ISMRAPIConnector {
 
     fetchServerInfo() {
         return new Promise<IServerInfo>((resolve, reject) => {
-
             fetch(ISMRAPIEndpoints.SERVER_INFO)
             .then(res => {
                 if (res.ok) {
