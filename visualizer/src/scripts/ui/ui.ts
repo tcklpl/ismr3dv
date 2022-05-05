8dcae30c914f691ca0a1c71d757eff4a741b0748
tcklpl <negroniluan@gmail.com>
2022-05-05 19:13:35 -0300

    buttons to save and export the session

--- visualizer/src/scripts/ui/ui.ts
+++ visualizer/src/scripts/ui/ui.ts
@@ -1,6 +1,7 @@
 import { UIConfig } from "./ui_config";
 import { UIInfo } from "./ui_info";
 import { UILoader } from "./ui_loader";
+import { UIOptionsHud } from "./ui_options_hud";
 import { UISession } from "./ui_session";
 import { UITimeline } from "./ui_timeline";
 
@@ -11,11 +12,13 @@ export class UI {
     private _loader = new UILoader();
     private _timeline = new UITimeline();
     private _session = new UISession();
+    private _optionsHud = new UIOptionsHud();
 
     registerEvents() {
         this._config.registerEvents();
         this._timeline.registerEvents();
         this._session.registerEvents();
+        this._optionsHud.registerEvents();
         this._info.update();
     }
 
@@ -37,6 +40,10 @@ export class UI {
 
     get session() {
         return this._session;
+    }
+
+    get optionsHud() {
+        return this._optionsHud;
     }
 
 }
---
+++ visualizer/src/scripts/ui/ui_options_hud.ts
@@ -0,0 +1,56 @@
+import { HTMLUtils } from "../visualizer/utils/html_utils";
+import { Visualizer } from "../visualizer/visualizer";
+import { ConfirmationScreen } from "./confirmation_screen";
+import { MessageScreen } from "./message_screen";
+
+export class UIOptionsHud {
+
+    private _saveSessionBtn!: JQuery<HTMLElement>;
+    private _exportSessionBtn!: JQuery<HTMLElement>;
+    private _timelineBtn!: JQuery<HTMLElement>;
+    private _newSessionBtn!: JQuery<HTMLElement>;
+
+    registerEvents() {
+        this._saveSessionBtn = $('#cfg-save-btn');
+        this._exportSessionBtn = $('#cfg-export-btn');
+        this._timelineBtn = $('#cfg-timeline-btn');
+        this._newSessionBtn = $('#cfg-session-btn');
+
+        this._saveSessionBtn.on('click', () => this.saveSession());
+        this._exportSessionBtn.on('click', () => this.exportSession());
+
+        this._newSessionBtn.on('click', () => {
+            Visualizer.instance.ui.session.resetUI();
+            if (Visualizer.instance.session) {
+                new ConfirmationScreen(
+                    'Are you sure?', 
+                    `<b>You are about to create a new session overwriting your current one, any unsaved changes will be lost.</b><br>
+                    <span class="text-secondary">You can safely cancel the session wizard until you fetch the API for data and this session will still
+                    be left intact, however if you query the API the session will be overwritten as soon as you get the
+                    data from the API</span>`, 
+                    () => $('#session-screen-container').collapse('show'));
+            } else {
+                $('#session-screen-container').collapse('show');
+            }
+        });
+    }
+
+    setSessionRelatedButtonsEnabled(state: boolean) {
+        this._saveSessionBtn.prop('disabled', !state);
+        this._exportSessionBtn.prop('disabled', !state);
+        this._timelineBtn.prop('disabled', !state);
+    }
+
+    private saveSession() {
+        alert('Not yet implemented');
+    }
+
+    private exportSession() {
+        if (!Visualizer.instance.session) {
+            new MessageScreen('Error', 'You are not currently in a session');
+            return;
+        }
+        const sess = Visualizer.instance.session;
+        HTMLUtils.downloadObjectAsFile(sess.asSerializable, sess.name + '.json');
+    }
+}
