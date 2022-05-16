1a311a2f3b7987895fe4bdc9cd7b39736d929c79
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-05-16 12:00:34 -0300

    session loading

--- visualizer/src/scripts/ui/ui_session.ts
+++ visualizer/src/scripts/ui/ui_session.ts
@@ -1,6 +1,8 @@
 import { IServerInfo } from "../visualizer/api/formats/i_server_info";
 import { ISMRSession } from "../visualizer/session/ismr_session";
+import { ISessionSave } from "../visualizer/session/i_session_save";
 import { Visualizer } from "../visualizer/visualizer";
+import { ConfirmationScreen } from "./confirmation_screen";
 import { MessageScreen } from "./message_screen";
 
 export class UISession {
@@ -8,6 +10,7 @@ export class UISession {
     private _serverInfo!: IServerInfo;
 
     private _tabsContainer!: JQuery<HTMLElement>;
+    private _cancelBtn!: JQuery<HTMLElement>;
 
     private _initialPanel!: JQuery<HTMLElement>;
     private _newFromApi!: JQuery<HTMLElement>;
@@ -31,6 +34,13 @@ export class UISession {
     private _finishSaveLocally!: JQuery<HTMLElement>;
     private _finishAutosave!: JQuery<HTMLElement>;
     private _finishAutosaveInterval!: JQuery<HTMLElement>;
+    private _finishFinalButton!: JQuery<HTMLElement>;
+    private _finishNoIDBLabel!: JQuery<HTMLElement>;
+
+    private _loadPanel!: JQuery<HTMLElement>;
+    private _loadIDBError!: JQuery<HTMLElement>;
+    private _loadEmpty!: JQuery<HTMLElement>;
+    private _loadStationList!: JQuery<HTMLElement>;
 
     private newScreenChangeHovered(newSelected?: JQuery<HTMLElement>) {
         this._initialPanel.children().removeClass('text-primary').addClass('text-secondary');
@@ -53,6 +63,7 @@ export class UISession {
         this._serverInfo = Visualizer.instance.serverInfo;
 
         this._tabsContainer = $('#session-screen-tabs');
+        this._cancelBtn = $('#ns-cancel-btn');
 
         this._initialPanel = $('#ns-initial-panel');
         this._newFromApi = $('#ns-new');
@@ -76,6 +87,15 @@ export class UISession {
         this._finishSaveLocally = $('#ns-finish-save-on-browser');
         this._finishAutosave = $('#ns-finish-autosave');
         this._finishAutosaveInterval = $('#ns-finish-autosave-interval');
+        this._finishFinalButton = $('#ns-btn-return-to-visualizer');
+        this._finishNoIDBLabel = $('#ns-finish-no-idb');
+
+        this._loadPanel = $('#ns-load-panel');
+        this._loadIDBError = $('#ns-load-idb-error');
+        this._loadEmpty = $('#ns-load-empty');
+        this._loadStationList = $('#ns-load-station-list');
+
+        this._cancelBtn.on('click', () => this.cancel());
 
         this._newFromApi.on('mouseover', () => this.newScreenChangeHovered(this._newFromApi)).on('click', () => this.newSessionFromAPI());
         this._newFromFile.on('mouseover', () => this.newScreenChangeHovered(this._newFromFile)).on('click', () => this.newSessionLoadFromFile());
@@ -97,6 +117,8 @@ export class UISession {
             if (Visualizer.instance.session) Visualizer.instance.session.config.auto_save_interval_minutes = this._finishAutosaveInterval.val() as number;
         });
 
+        this._finishFinalButton.on('click', () => Visualizer.instance.session?.save());
+
         if (this._serverInfo.showcase_mode) {
             this.switchDatePanelTab(this._datePanelTabShowcase);
         }
@@ -111,6 +133,22 @@ export class UISession {
         this._finishSaveLocally.prop('checked', true);
         this._finishAutosave.prop('checked', true);
         this._finishAutosaveInterval.val(5);
+        this._cancelBtn.show();
+
+        if (!Visualizer.instance.idb.isAvailable) {
+            this._finishSaveLocally.removeAttr('checked');
+            this._finishSaveLocally.prop('disabled', true);
+            this._finishAutosave.removeAttr('checked');
+            this._finishAutosave.prop('disabled', true);
+            this._finishAutosaveInterval.prop('disabled', true);
+            this._finishNoIDBLabel.removeClass('d-none').addClass('row');
+        }
+    }
+    
+    private cancel() {
+        new ConfirmationScreen('Are you sure?', 'You are about to cancel the session creation, everything will be discarded.', () => {
+            $('#session-screen-container').collapse('hide');
+        });
     }
 
     private newSessionFromAPI() {
@@ -119,7 +157,9 @@ export class UISession {
     }
 
     private newSessionLoadFromFile() {
-        alert('Not yet implemented');
+        this.constructLoadSessionPanel();
+        this.setActivePanel(this._loadPanel);
+        this.setProgressBarCompletion(2);
     }
 
     private newSessionCustomData() {
@@ -165,9 +205,8 @@ export class UISession {
                 if (list.length > 0) {
                     const session = new ISMRSession(startDate, endDate, this._nameField.val() as string);
                     session.stations = list;
-                    this.showFinalPanel();
                     Visualizer.instance.session = session;
-                    Visualizer.instance.ui.optionsHud.setSessionRelatedButtonsEnabled(true);
+                    this.showFinalPanel();
                 } else {
                     new MessageScreen('Error', 'No available stations were found on the provided time interval');
                     this.switchDatePanelTab(this._serverInfo.showcase_mode ? this._datePanelTabShowcase : this._datePanelTabSelectors);
@@ -180,7 +219,79 @@ export class UISession {
     }
 
     showFinalPanel() {
+        this._cancelBtn.hide();
+
+        const session = Visualizer.instance.session;
+        if (session) {
+            this._finishSaveLocally.prop('checked', session.config.save_on_browser);
+            this._finishAutosave.prop('checked', session.config.auto_save);
+            this._finishAutosaveInterval.val(session.config.auto_save_interval_minutes);
+        }
+
         this.setProgressBarCompletion(4);
         this.setActivePanel(this._finishPanel);
+        Visualizer.instance.ui.optionsHud.setSessionRelatedButtonsEnabled(true);
+    }
+
+    private createLoadedSessionCard(save: ISessionSave) {
+        const creationDate = save.creation_date.toLocaleString();
+        const startDate = save.start_date.toLocaleString();
+        const endDate = save.end_date.toLocaleString();
+
+        return `
+        <div class="card ns-hoverable" style="cursor: pointer; margin-right: 0.5em; margin-bottom: 0.5em;">
+            <div class="card-body h-100 container-fluid">
+                <div>
+                    <i class="bi-hdd-fill" style="font-size: 1.5em; margin-right: 0.5em; vertical-align: middle;"></i>${save.name}
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-10 d-flex flex-column h-100">
+                        <span title="Creation Date"><i class="bi-calendar-event icon-left"></i>${creationDate}</span>
+                        <span title="Date Range"><i class="bi-calendar-range icon-left"></i>${startDate} - ${endDate}</span>
+                    </div>
+                    <div class="col-2">
+                        <i class="bi-broadcast-pin icon-left"></i>${save.station_list.length}
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+    private constructLoadSessionPanel() {
+
+        const setLoadPanel = (panel: JQuery<HTMLElement>) => {
+            this._loadIDBError.removeClass('d-flex active show').addClass('d-none');
+            this._loadEmpty.removeClass('d-flex active show').addClass('d-none');
+            this._loadStationList.removeClass('d-flex active show').addClass('d-none');
+            panel.removeClass('d-none').addClass('d-flex active show');
+        };
+
+        Visualizer.instance.idb.sessionController.fetchAll().then(stations => {
+            if (stations.length > 0) {
+                this._loadStationList.empty();
+                stations.forEach(s => {
+                    const src = this.createLoadedSessionCard(s);
+                    const html = $.parseHTML(src);
+                    $(html).on('click', () => {
+                        this.loadSession(s);
+                    });
+                    this._loadStationList.append(html);
+                });
+                setLoadPanel(this._loadStationList);
+            } else {
+                setLoadPanel(this._loadEmpty);
+            }
+        })
+        .catch(() => {
+            setLoadPanel(this._loadIDBError);
+        });
+    }
+
+    private loadSession(save: ISessionSave) {
+        const session = ISMRSession.constructFromSave(save);
+        Visualizer.instance.session = session;
+        this.showFinalPanel();
     }
 }
