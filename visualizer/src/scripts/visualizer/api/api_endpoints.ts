a71de725c02bc1789ebe2c0ed361e5c853ffaf7b
tcklpl <negroniluan@gmail.com>
2022-05-21 11:14:34 -0300

    visualizer ipp fetching

--- visualizer/src/scripts/visualizer/api/api_endpoints.ts
+++ visualizer/src/scripts/visualizer/api/api_endpoints.ts
@@ -2,6 +2,7 @@
 export class ISMRAPIEndpoints {
 
     static STATION_LIST = 'api/stations/:startdate/:enddate';
+    static IPP = 'api/ipp/:startdate/:enddate/:sat/:stations/:ion';
     static SERVER_INFO = 'api/info';
 
 }
