987c0603aa73f8757b42f7d4ae4d06f5e402f79d
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-05-24 21:19:46 -0300

    tropic gizmos

---
+++ visualizer/src/scripts/visualizer/gizmos/gz_capricorn.ts
@@ -0,0 +1,16 @@
+import { Gizmo } from "../../engine/data_formats/gizmos/gizmo";
+import { LineLoop } from "../../engine/data_formats/mesh/line_loop";
+import { Vec3 } from "../../engine/data_formats/vec/vec3";
+import { MUtils } from "../../engine/utils/math_utils";
+
+export class GizmoCapricorn extends Gizmo {
+
+    constructor() {
+        const points: Vec3[] = [];
+        for (let i = -180; i <= 180; i++) {
+            points.push(MUtils.latLongToUnitSphere(-23.5, i).multiplyByFactor(1.001));
+        }
+        super('tropic of capricorn', new LineLoop(points));
+    }
+
+}
