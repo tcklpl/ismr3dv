386f9d4b9c8e6be42516f6da5f87746fb9a2ec24
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-06-04 12:50:18 -0300

    timeline base

--- visualizer/src/scripts/ui/ui_timeline.ts
+++ visualizer/src/scripts/ui/ui_timeline.ts
@@ -1,3 +1,5 @@
+import { ISMRSession } from "../visualizer/session/ismr_session";
+import { RandomUtils } from "../visualizer/utils/random_utils";
 import { Visualizer } from "../visualizer/visualizer";
 import { IUI } from "./i_ui";
 import { MessageScreen } from "./message_screen";
@@ -12,6 +14,20 @@ export class UITimeline implements IUI {
     private _needFetchBtn = $('#timeline-btn-fetch');
     private _fetchingPanel = $('#timeline-fetching');
 
+    private _timelinePanel = $('#timeline-panel');
+    private _timelineKeyframesUl = $('#timeline-keyframes');
+    private _timelineChildren: JQuery<HTMLElement>[] = [];
+    private _tlMomentName = $('#tl-cur-name');
+
+    private _tlBtnFirst = $('#tl-btn-first');
+    private _tlBtnPrev = $('#tl-btn-prev');
+    private _tlBtnNext = $('#tl-btn-next');
+    private _tlBtnLast = $('#tl-btn-last');
+    private _tlMomentCounter = $('#tl-cur-number');
+    private _tlDisplayMaxMoments = $('#tl-max-number');
+
+    private _activeMoment = 0;
+
     private setActivePanel(panel: JQuery<HTMLElement>) {
         this._container.children().removeClass('d-flex show').addClass('d-none');
         panel.removeClass('d-none').addClass('d-flex show');
@@ -22,6 +38,12 @@ export class UITimeline implements IUI {
             this.setActivePanel(this._fetchingPanel);
             this.fetchIPP();
         });
+
+        this._tlBtnFirst.on('click', () => this.setMoment(0));
+        this._tlBtnPrev.on('click', () => this.previousMoment());
+        this._tlBtnNext.on('click', () => this.nextMoment());
+        this._tlBtnLast.on('click', () => this.setMoment(this.session.timeline.currentMoments.length - 1));
+        this._tlMomentCounter.on('change', () => this.setMoment(this._tlMomentCounter.val() as number - 1));
     }
 
     updateForSelectedStations() {
@@ -37,6 +59,12 @@ export class UITimeline implements IUI {
             this.setActivePanel(this._needFetchPanel);
             return;
         }
+
+        session.timeline.updateSelectedStations(session.selectedStations.map(s => s.station_id));
+        this.constructKeyframes();
+        this._tlMomentCounter.prop('max', session.timeline.currentMoments.length);
+        this._tlDisplayMaxMoments.html(`/ ${session.timeline.currentMoments.length}`);
+        this.setActivePanel(this._timelinePanel);
     }
 
     fetchIPP() {
@@ -53,6 +81,7 @@ export class UITimeline implements IUI {
         .then(t => {
             session.addIPP(t.map(ipp => {
                 ipp.id = parseInt(`${ipp.id}`);
+                ipp.value = parseInt(`${ipp.value}`);
                 return ipp;
             }));
             this.updateForSelectedStations();
@@ -62,6 +91,81 @@ export class UITimeline implements IUI {
             new MessageScreen('Error', 'There was an error while fetching the requested data, does it exist? is your connection ok?');
             console.log(err);
         });
+    }
+
+    private updateInterfaceToMoment() {
+        const backwardsDisabled = this._activeMoment == 0;
+        const forwardsDisabled = this._activeMoment == this.session.timeline.currentMoments.length - 1;
+        this._tlBtnFirst.prop('disabled', backwardsDisabled);
+        this._tlBtnPrev.prop('disabled', backwardsDisabled);
+        this._tlBtnNext.prop('disabled', forwardsDisabled);
+        this._tlBtnLast.prop('disabled', forwardsDisabled);
+        this._tlMomentCounter.val(this._activeMoment + 1);
+        this._tlMomentName.html(this.session.timeline.currentMoments[this._activeMoment].date.toLocaleString());
+    }
+
+    setMoment(i: number) {
+        if (i > this.session.timeline.currentMoments.length - 1 || i < 0) return;
+        this.setActiveMoment(i);
+        this.ippSphere.currentTexture = this.session.timeline.buffer.setMomentByIndex(i);
+        this.ippSphere.currentChannel = this._activeMoment % 4;
+    }
+
+    nextMoment() {
+        if (this._activeMoment >= this.session.timeline.currentMoments.length - 1) return;
+        this.setActiveMoment(this._activeMoment + 1);
+        this.ippSphere.currentTexture = this.session.timeline.buffer.nextMoment();
+        this.ippSphere.currentChannel = this._activeMoment % 4;
+    }
+
+    previousMoment() {
+        if (this._activeMoment <= 0) return;
+        this.setActiveMoment(this._activeMoment - 1);
+        this.ippSphere.currentTexture = this.session.timeline.buffer.previousMoment();
+        this.ippSphere.currentChannel = this._activeMoment % 4;
+    }
+
+    setActiveMoment(index: number) {
+        this._timelineChildren[this._activeMoment].removeClass('active');
+        this._activeMoment = index;
+        this._timelineChildren[index].addClass('active');
+
+        this._timelineChildren.forEach(c => c.removeClass('buffered same-buffer'));
+        const startI = Math.floor(index / 4) * 4;
+        for (let i = Math.max(0, startI - 8); i <= Math.min(this.session.timeline.currentMoments.length - 1, startI + 11); i++) {
+            if (!this._timelineChildren[i].hasClass('active')) {
+                this._timelineChildren[i].addClass(i >= startI && i < startI + 4 ? 'same-buffer' : 'buffered');
+            }
+        }
+        this._timelineChildren[index][0].scrollIntoView(true);
+        this.updateInterfaceToMoment();
+    }
+
+    constructKeyframes() {
+        this._timelineKeyframesUl.empty();
+
+        this.session.timeline.currentMoments.forEach((m, i) => {
+            const id = `tlid-${RandomUtils.randomString(10)}`;
+            const src = `<li id="${id}"></li>`;
+            const html = $.parseHTML(src);
+            $(html).on('click', () => this.setMoment(i));
+            $(html).on('mouseover', () => {
+                this._tlMomentName.html(m.date.toLocaleString());
+                this._tlMomentCounter.val(i + 1);
+            });
+            $(html).on('mouseleave', () => this.updateInterfaceToMoment());
+            this._timelineKeyframesUl.append(html);
+            this._timelineChildren.push($(`#${id}`));
+        });
+        this.setActiveMoment(0);
+    }
+
+    private get session() {
+        return Visualizer.instance.session as ISMRSession;
+    }
+
+    private get ippSphere() {
+        return Visualizer.instance.universeScene.ippSphere;
     }
 
 }
--- visualizer/src/scripts/visualizer/session/session_timeline.ts
+++ visualizer/src/scripts/visualizer/session/session_timeline.ts
@@ -1,4 +1,8 @@
+import { Vec2 } from "../../engine/data_formats/vec/vec2";
 import { IIPPInfo } from "../api/formats/i_ipp_info";
+import { Moment } from "./moment";
+import { MomentBufferingManager } from "./moment_buffering_manager";
+import { TimelineImageBuffers } from "./timeline_image_buffers";
 
 export class SessionTimeline {
     
@@ -6,32 +10,38 @@ export class SessionTimeline {
     private _ippByDate: Map<number, IIPPInfo[]> = new Map(); // number is Date#getTime() / 100000. Cannot use Date object for comparisions
     private _coveredStations: number[] = [];
 
+    private _currentlySelectedStations: number[] = [];
+    private _moments: Moment[] = [];
+    private _timelineImageBuffers = new TimelineImageBuffers(new Vec2(2048, 1024));
+    private _momentBufferingManager = new MomentBufferingManager(this._timelineImageBuffers, 0.01);
+
     constructor() {        
+    }
+
+    private getTimeFromIPP(ipp: IIPPInfo) {
+        // time_utc comes as "2014-09-01 22:00:00" (UTC)
+        const regex = /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/;
+        const matches = regex.exec(ipp.time_utc);
+        if (!matches || matches.length < 7) {
+            console.warn(`BAD IPP TIME UTC: ${ipp.time_utc}`);
+            return -1;
+        }
+        return Date.UTC(parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3]), parseInt(matches[4]), parseInt(matches[5]), parseInt(matches[6]));
     }
 
     addIPP(list: IIPPInfo[]) {
         const filteredList = list.filter(x => !this._ippInfoList.includes(x));
-        console.log(filteredList);
         filteredList.forEach(ipp => {
-            // time_utc comes as "2014-09-01 22:00:00" (UTC)
-            const regex = /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/;
-            const matches = regex.exec(ipp.time_utc);
-
-            if (!matches || matches.length < 7) {
-                console.warn(`BAD IPP TIME UTC: ${ipp.time_utc}`);
-            } else {
-                const date = Date.UTC(parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3]), parseInt(matches[4]), parseInt(matches[5]), parseInt(matches[6]));
-                let toInsert = this._ippByDate.get(Math.floor(date / 100000));
-                if (!toInsert) {
-                    toInsert = [];
-                    this._ippByDate.set(Math.floor(date / 100000), toInsert);
-                }
-                toInsert.push(ipp);
-                this._ippInfoList.push(ipp);
-                if (!this._coveredStations.includes(ipp.id))
-                    this._coveredStations.push(ipp.id);
+            const date = this.getTimeFromIPP(ipp);
+            let toInsert = this._ippByDate.get(Math.floor(date / 100000));
+            if (!toInsert) {
+                toInsert = [];
+                this._ippByDate.set(Math.floor(date / 100000), toInsert);
             }
-            
+            toInsert.push(ipp);
+            this._ippInfoList.push(ipp);
+            if (!this._coveredStations.includes(ipp.id))
+                this._coveredStations.push(ipp.id);
         });
     }
 
@@ -53,12 +63,38 @@ export class SessionTimeline {
         return true;
     }
 
+    updateSelectedStations(selection: number[]) {
+        // return false if any selected station is not covered, however this shouldn't happen
+        if (selection.find(x => !this._coveredStations.includes(Math.floor(x)))) return false;
+
+        // return true if the 'new' selection is equal to the current one
+        if (this._currentlySelectedStations.every(x => selection.includes(Math.floor(x))) && selection.every(x => this._currentlySelectedStations.includes(Math.floor(x)))) return true;
+
+        // selection is different...
+        this._currentlySelectedStations = selection.map(x => Math.floor(x));
+        this._moments = [];
+        this._ippByDate.forEach((ippList, time) => {
+            const filteredIPPList = ippList.filter(x => this._currentlySelectedStations.includes(Math.floor(x.id)));
+            this._moments.push(new Moment(time, filteredIPPList));
+        });
+        this._momentBufferingManager.replaceMoments([...this._moments]);
+        return true;
+    }
+
     get coverage() {
         return [...this._ippByDate.keys()];
     }
 
+    get currentMoments() {
+        return this._moments;
+    }
+
     get ippList() {
         return this._ippInfoList;
+    }
+
+    get buffer() {
+        return this._momentBufferingManager;
     }
 
 }
