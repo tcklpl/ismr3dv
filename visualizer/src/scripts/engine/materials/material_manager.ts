1756091c70ff53a495c2ede65f079714ad486654
tcklpl <negroniluan@gmail.com>
2022-07-22 21:49:33 -0300

    assert material function

--- visualizer/src/scripts/engine/materials/material_manager.ts
+++ visualizer/src/scripts/engine/materials/material_manager.ts
@@ -1,3 +1,4 @@
+import { EngineError } from "../errors/engine_error";
 import { GenericManager } from "../generic_manager";
 import { Material } from "./material";
 
@@ -5,6 +6,12 @@ export class MaterialManager extends GenericManager<Material> {
 
     public getByName(name: string) {
         return this.allRegistered.find(x => x.name == name);
+    }
+
+    assertGetByName(name: string) {
+        const res = this.getByName(name);
+        if (!res) throw new EngineError('Material Manager', `Failed to get material '${name}'`);
+        return res;
     }
 
 }
