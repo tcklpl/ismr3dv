ac52d4f3c5014ac0bc297102841eeb12d4754ef1
tcklpl <negroniluan@gmail.com>
2022-07-25 12:57:37 -0300

    utils to save blob and hex to vec3

--- visualizer/src/scripts/visualizer/utils/html_utils.ts
+++ visualizer/src/scripts/visualizer/utils/html_utils.ts
@@ -1,3 +1,4 @@
+import { Vec3 } from "../../engine/data_formats/vec/vec3";
 
 export class HTMLUtils {
 
@@ -9,6 +10,28 @@ export class HTMLUtils {
         body.appendChild(a);
         a.click();
         body.removeChild(a);
+    }
+
+    static saveBlob(blob: Blob | null, fileName: string) {
+        if (!blob) {
+            console.warn('Trying to save empty blob');
+            return;
+        }
+        const body = document.body;
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.setAttribute('download', fileName);
+        body.appendChild(a);
+        a.click();
+        body.removeChild(a);
+    }
+
+    static hexToVec3(hex: string) {
+        const val = hex.replace('#', '');
+        const cr = parseInt(val.substring(0, 2), 16) / 255;
+        const cg = parseInt(val.substring(2, 4), 16) / 255;
+        const cb = parseInt(val.substring(4, 6), 16) / 255;
+        return new Vec3(cr, cg, cb);
     }
 
 }
