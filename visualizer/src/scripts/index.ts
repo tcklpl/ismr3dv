a8fffcaecf802aff35acd630c6bd1d04f013d902
tcklpl <negroniluan@gmail.com>
2022-08-02 12:05:15 -0300

    visualizer instancing after extension enabling

--- visualizer/src/scripts/index.ts
+++ visualizer/src/scripts/index.ts
@@ -6,8 +6,6 @@ const glCtx = (canvas.get(0) as HTMLCanvasElement).getContext('webgl2');
 if (!glCtx) throw new EngineError('Engine', `Failed to acquire WebGL2 context`);
 globalThis.gl = glCtx;
 
-new Visualizer();
-
 /*
     Make sure we have both EXT_color_buffer_float and OES_texture_float_linear extensions in order to have HDR rendering.
 */
@@ -15,6 +13,8 @@ if (!gl.getExtension("EXT_color_buffer_float"))
     throw new EngineError('Engine', 'Extension EXT_color_buffer_float is not supported by your system');
 if (!gl.getExtension("OES_texture_float_linear")) 
     throw new EngineError('Engine', 'Extension OES_texture_float_linear is not supported by your system');
+
+new Visualizer();
 
 canvas.on('mousedown', e => {
     if (!visualizer.universeScene.isHoveringOverStation)
