f8a74cdfee37006a34eabd72762bc53e5e265d79
tcklpl <negroniluan@gmail.com>
2022-08-24 14:11:58 -0300

    new timeline ui

--- visualizer/src/scripts/ui/ui_timeline.ts
+++ visualizer/src/scripts/ui/ui_timeline.ts
@@ -1,4 +1,8 @@
+import { Vec2 } from "../engine/data_formats/vec/vec2";
+import { MUtils } from "../engine/utils/math_utils";
 import { ISMRSession } from "../visualizer/session/ismr_session";
+import { MomentFreeingLocation } from "../visualizer/session/moments/moment_buffering_manager";
+import { DateUtils } from "../visualizer/utils/date_utils";
 import { RandomUtils } from "../visualizer/utils/random_utils";
 import { IUI } from "./i_ui";
 import { MessageScreen } from "./message_screen";
@@ -14,18 +18,21 @@ export class UITimeline implements IUI {
     private _fetchingPanel = $('#timeline-fetching');
 
     private _timelinePanel = $('#timeline-panel');
-    private _timelineKeyframesUl = $('#timeline-keyframes');
-    private _timelineChildren: JQuery<HTMLElement>[] = [];
-    private _tlMomentName = $('#tl-cur-name');
+    private _timelineBarBg = $('#tl-bar-background');
 
-    private _tlBtnFirst = $('#tl-btn-first');
-    private _tlBtnPrev = $('#tl-btn-prev');
-    private _tlBtnNext = $('#tl-btn-next');
-    private _tlBtnLast = $('#tl-btn-last');
-    private _tlMomentCounter = $('#tl-cur-number');
-    private _tlDisplayMaxMoments = $('#tl-max-number');
+    private _tlActiveMarker = $('#tl-bar-active');
+    private _tlGhostMarker = $('#tl-bar-ghost');
+    private _tlBufferBar = $('#tl-bar-buffered');
+    private _tlColorBar = $('#tl-bar-colored');
+
+    private _tlMomentName = $('#tl-controls-name');
 
     private _activeMoment = 0;
+    private _hoveredMoment = 0;
+    private _timelineMode: 'idle' | 'user-hovering' | 'playing' = 'idle';
+    
+    private _bufferBounds = new Vec2(0, 0);
+    private _colorBounds = new Vec2(0, 0);
 
     private setActivePanel(panel: JQuery<HTMLElement>) {
         this._container.children().removeClass('d-flex show').addClass('d-none');
@@ -38,11 +45,130 @@ export class UITimeline implements IUI {
             this.fetchIPP();
         });
 
-        this._tlBtnFirst.on('click', () => this.setMoment(0));
-        this._tlBtnPrev.on('click', () => this.previousMoment());
-        this._tlBtnNext.on('click', () => this.nextMoment());
-        this._tlBtnLast.on('click', () => this.setMoment(this.session.timeline.currentMoments.length - 1));
-        this._tlMomentCounter.on('change', () => this.setMoment(this._tlMomentCounter.val() as number - 1));
+        this._timelineBarBg.on('click', e => {
+            const prog = e.offsetX / (this._timelineBarBg.width() as number);
+            this._activeMoment = Math.round(prog * (this.session.timeline.currentMoments.length - 1));
+            this.updateCurrentMomentMarkerAndInfo();
+            this.session.timeline.buffer.getMomentByIndex(this._activeMoment);
+            visualizer.ui.bottomHud.currentDateLabel = this.session.timeline.currentMoments[this._activeMoment].date.toLocaleString();
+        });
+
+        this._timelineBarBg.on('mousemove', e => {
+            const prog = e.offsetX / (this._timelineBarBg.width() as number);
+            this._hoveredMoment = Math.round(prog * (this.session.timeline.currentMoments.length - 1));
+            this._timelineMode = 'user-hovering';
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+
+        this._timelineBarBg.on('mouseleave', e => {
+            this._timelineMode = 'idle';
+            this._hoveredMoment = -1;
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+
+        visualizer.events.on('moment-buffered', (args: any[]) => {
+            const moment = args[0] as number;
+            if (moment < this._bufferBounds.x) this._bufferBounds.x = moment;
+            if (moment > this._bufferBounds.y) this._bufferBounds.y = moment;
+            this.updateBufferIndicators();
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+
+        visualizer.events.on('moment-colored', (args: any[]) => {
+            const moment = args[0] as number;
+            if (moment < this._colorBounds.x) this._colorBounds.x = moment;
+            if (moment > this._colorBounds.y) this._colorBounds.y = moment;
+            this.updateBufferIndicators();
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+
+        visualizer.events.on('timeline-buffer-bounds-update', (args: any[]) => {
+            const bounds = args[0] as Vec2;
+            const alreadyBuffered = args[1] as Vec2;
+
+            this._bufferBounds.x = bounds.x;
+            this._bufferBounds.y = Math.min(alreadyBuffered.x, bounds.y);
+
+            this._colorBounds.x = this._bufferBounds.x;
+            this._colorBounds.y = this._bufferBounds.y;
+
+            this.updateBufferIndicators();
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+
+        visualizer.events.on('moment-interpolation-complete', (args: any[]) => {
+            const bounds = args[0] as Vec2;
+            this._bufferBounds = bounds.clone();
+
+            this.updateBufferIndicators();
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+
+        visualizer.events.on('all-moments-processed', (args: any[]) => {
+            const bounds = args[0] as Vec2;
+            this._bufferBounds = bounds.clone();
+            this._colorBounds = this._bufferBounds.clone();
+
+            this.updateBufferIndicators();
+            this.updateCurrentMomentMarkerAndInfo();
+        });
+    }
+
+    getMomentOffsetPercentage(index: number) {
+        const total = this.session.timeline.currentMoments.length;
+        if (index < 0 || index > (total - 1)) {
+            console.warn(`Invalid index for moment offset. Trying to set index ${index} for a list of size ${total}`);
+            return 0;
+        }
+        return index / total * 100;
+    }
+
+    updateCurrentMomentMarkerAndInfo() {
+        let momentToQuery = 0;
+        let updateGhost = true;
+        switch(this._timelineMode) {
+            case 'idle':
+            case 'playing':
+                momentToQuery = this._activeMoment;
+                break;
+            case 'user-hovering':
+                momentToQuery = this._hoveredMoment;
+                updateGhost = false;
+                break;
+        }
+        if (momentToQuery < 0 || momentToQuery > (this.session.timeline.currentMoments.length - 1)) {
+            console.warn(`Failed to update to moment ${momentToQuery}: Out of bounds (0, ${this.session.timeline.currentMoments.length - 1})`);
+            return;
+        }
+        const curPercentage = this.getMomentOffsetPercentage(momentToQuery);
+        this._tlActiveMarker.css('left', `${curPercentage}%`);
+
+        if (updateGhost) {
+            this._tlGhostMarker.css('left', `${curPercentage}%`);
+        }
+
+        const moment = this.session.timeline.currentMoments[momentToQuery];
+
+        const bufferStatus = 
+            moment.bufferProgression == 0 ? 'Not Buffered' :
+            moment.bufferProgression == 1 ? 'Interpolated' :
+            moment.bufferProgression == 2 ? 'Buffered' : 'ERROR';
+        this._tlMomentName.html(`
+            <span><i class="bi-alarm icon-left"></i>${DateUtils.to_DDMMYYYY_HHMMSS(moment.date)}</span>
+            <span><i class="bi-bar-chart icon-left"></i>${moment.avgDataValue.toFixed(2)}</span>
+            <span><i class="bi-memory icon-left"></i>${bufferStatus}</span>`
+        );
+    }
+
+    updateBufferIndicators() {
+        const bufferStart = this.getMomentOffsetPercentage(this._bufferBounds.x);
+        const bufferEnd = this.getMomentOffsetPercentage(this._bufferBounds.y);
+
+        const colorStart = this.getMomentOffsetPercentage(this._colorBounds.x);
+        const colorEnd = this.getMomentOffsetPercentage(this._colorBounds.y);
+
+        this._tlBufferBar.css('left', `${bufferStart}%`).css('width', `${bufferEnd - bufferStart}%`);
+        this._tlColorBar.css('left', `${colorStart}%`).css('width', `${colorEnd - colorStart}%`);
     }
 
     updateForSelectedStations() {
@@ -60,10 +186,44 @@ export class UITimeline implements IUI {
         }
 
         session.timeline.updateSelectedStations(session.selectedStations.map(s => s.station_id));
-        this.constructKeyframes();
-        this._tlMomentCounter.prop('max', session.timeline.currentMoments.length);
-        this._tlDisplayMaxMoments.html(`/ ${session.timeline.currentMoments.length}`);
+        this.constructMomentValueIndicators();
         this.setActivePanel(this._timelinePanel);
+        this.updateBufferIndicators();
+        this.updateCurrentMomentMarkerAndInfo();
+    }
+
+    private constructMomentValueIndicators() {
+        const session = visualizer.session;
+        if (!session) return;
+
+        const width = moment2DCanvas.canvas.getBoundingClientRect().width;
+        const height = moment2DCanvas.canvas.getBoundingClientRect().height;
+
+        // For some reason the canvas stays at 0,0 for the first run
+        if (width == 0 && height == 0) {
+            setTimeout(() => this.constructMomentValueIndicators(), 1000);
+        }
+
+        moment2DCanvas.canvas.width = width;
+        moment2DCanvas.canvas.height = height;
+
+        const stepX = Math.round(width / session.timeline.currentMoments.length);
+
+        moment2DCanvas.clearRect(0, 0, width, height);
+        const path = new Path2D();
+        path.moveTo(0, height);
+
+        MUtils.normalizeListMinMax(session.timeline.currentMoments.map(m => m.avgDataValue)).forEach((m, i) => {
+            const y = height - Math.round(m * height);
+            path.lineTo(stepX * i, y);
+        });
+
+        path.lineTo(width, height);
+        path.closePath();
+        
+        moment2DCanvas.fillStyle = '#555';
+        moment2DCanvas.fill(path);
+        
     }
 
     fetchIPP() {
@@ -90,73 +250,6 @@ export class UITimeline implements IUI {
             new MessageScreen('Error', 'There was an error while fetching the requested data, does it exist? is your connection ok?');
             console.log(err);
         });
-    }
-
-    private updateInterfaceToMoment() {
-        const backwardsDisabled = this._activeMoment == 0;
-        const forwardsDisabled = this._activeMoment == this.session.timeline.currentMoments.length - 1;
-        this._tlBtnFirst.prop('disabled', backwardsDisabled);
-        this._tlBtnPrev.prop('disabled', backwardsDisabled);
-        this._tlBtnNext.prop('disabled', forwardsDisabled);
-        this._tlBtnLast.prop('disabled', forwardsDisabled);
-        this._tlMomentCounter.val(this._activeMoment + 1);
-        this._tlMomentName.html(this.session.timeline.currentMoments[this._activeMoment].date.toLocaleString());
-    }
-
-    setMoment(i: number) {
-        if (i > this.session.timeline.currentMoments.length - 1 || i < 0) return;
-        this.setActiveMoment(i);
-        this.ippSphere.currentTexture = this.session.timeline.buffer.setMomentByIndex(i);
-        this.ippSphere.currentChannel = this._activeMoment % 4;
-    }
-
-    nextMoment() {
-        if (this._activeMoment >= this.session.timeline.currentMoments.length - 1) return;
-        this.setActiveMoment(this._activeMoment + 1);
-        this.ippSphere.currentTexture = this.session.timeline.buffer.nextMoment();
-        this.ippSphere.currentChannel = this._activeMoment % 4;
-    }
-
-    previousMoment() {
-        if (this._activeMoment <= 0) return;
-        this.setActiveMoment(this._activeMoment - 1);
-        this.ippSphere.currentTexture = this.session.timeline.buffer.previousMoment();
-        this.ippSphere.currentChannel = this._activeMoment % 4;
-    }
-
-    setActiveMoment(index: number) {
-        this._timelineChildren[this._activeMoment].removeClass('active');
-        this._activeMoment = index;
-        this._timelineChildren[index].addClass('active');
-
-        this._timelineChildren.forEach(c => c.removeClass('buffered same-buffer'));
-        const startI = Math.floor(index / 4) * 4;
-        for (let i = Math.max(0, startI - 8); i <= Math.min(this.session.timeline.currentMoments.length - 1, startI + 11); i++) {
-            if (!this._timelineChildren[i].hasClass('active')) {
-                this._timelineChildren[i].addClass(i >= startI && i < startI + 4 ? 'same-buffer' : 'buffered');
-            }
-        }
-        this._timelineChildren[index][0].scrollIntoView(true);
-        this.updateInterfaceToMoment();
-    }
-
-    constructKeyframes() {
-        this._timelineKeyframesUl.empty();
-
-        this.session.timeline.currentMoments.forEach((m, i) => {
-            const id = `tlid-${RandomUtils.randomString(10)}`;
-            const src = `<li id="${id}"></li>`;
-            const html = $.parseHTML(src);
-            $(html).on('click', () => this.setMoment(i));
-            $(html).on('mouseover', () => {
-                this._tlMomentName.html(m.date.toLocaleString());
-                this._tlMomentCounter.val(i + 1);
-            });
-            $(html).on('mouseleave', () => this.updateInterfaceToMoment());
-            this._timelineKeyframesUl.append(html);
-            this._timelineChildren.push($(`#${id}`));
-        });
-        this.setActiveMoment(0);
     }
 
     private get session() {
