b9cf04dbcf935c5aaca0dd90d13122867fcde20c
tcklpl <negroniluan@gmail.com>
2022-08-24 14:19:38 -0300

    sun alignment with time - still broken

--- visualizer/src/scripts/visualizer/universe_scene.ts
+++ visualizer/src/scripts/visualizer/universe_scene.ts
@@ -1,5 +1,6 @@
 import { Vec3 } from "../engine/data_formats/vec/vec3";
 import { Scene } from "../engine/scenes/scene";
+import { MUtils } from "../engine/utils/math_utils";
 import { MainCamera } from "./camera/main_camera";
 import { EarthEntity } from "./objects/earth";
 import { IPPSphereEntity } from "./objects/ipp_renderer";
@@ -31,16 +32,32 @@ export class UniverseScene extends Scene {
         this._earth = this._objectManager.summon("earth", EarthEntity);
 
         this._sun = this._objectManager.summon("sun", SunEntity);
-        this._sun.translate(new Vec3(8, 0, 8));
+        // sun starts at 8 * sin0, 0, 8*cos0
+        this._sun.translate(new Vec3(0, 0, 8));
 
         this._ipp = this._objectManager.summon("ipp_sphere", IPPSphereEntity);
-        this._ipp.scale(new Vec3(0.02, 0.02, 0.02));
+        this._ipp.scale(Vec3.fromValue(0.06));
 
         this._earth.registerChildren(this._ipp);
 
         this._skybox = new Skybox(this._materialManager.assertGetByName('stars'), this._shaderManager.assertGetShader('skybox'));
 
         this.addObjects(this._earth, this._sun, this._ipp);
+        this.alignSunWithTime(new Date());
+    }
+
+    alignSunWithTime(t: Date) {
+        // sun's defualt position @0,0,8 sits around GMT+6
+        const utcHour = t.getUTCHours();
+        const utcMins = t.getUTCMinutes();
+        // 360 / 24 = each hour = 15 degrees
+        const offsetHours = (utcHour + 6) % 24;
+        const rotationDegrees = (offsetHours + (utcMins / 60)) * 15;
+        const rotationDegreesRadians = MUtils.degToRad(rotationDegrees);
+        const x = Math.sin(rotationDegreesRadians) * 16;
+        const z = Math.cos(rotationDegreesRadians) * 16;
+        this._sun.setPosition(new Vec3(x, 0, z));
+        visualizer.ui.bottomHud.currentDateLabel = t.toLocaleString();
     }
 
     get earth() {
