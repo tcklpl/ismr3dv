8b62b0e27644f59a71874d6a09cf66995c5eff7f
tcklpl <negroniluan@gmail.com>
2022-08-24 14:12:16 -0300

    timeline 2d canvas globals

--- visualizer/src/scripts/global_declarations.d.ts
+++ visualizer/src/scripts/global_declarations.d.ts
@@ -3,4 +3,5 @@ import { Visualizer } from "./visualizer/visualizer";
 declare global {
     var gl: WebGL2RenderingContext;
     var visualizer: Visualizer;
+    var moment2DCanvas: CanvasRenderingContext2D;
 }
--- visualizer/src/scripts/index.ts
+++ visualizer/src/scripts/index.ts
@@ -1,19 +1,25 @@
 import { EngineError } from "./engine/errors/engine_error";
 import { Visualizer } from "./visualizer/visualizer";
 
+// First acquire the WebGL2 Rendering Context
 const canvas = $('#ismr3dcanvas');
 const glCtx = (canvas.get(0) as HTMLCanvasElement).getContext('webgl2');
 if (!glCtx) throw new EngineError('Engine', `Failed to acquire WebGL2 context`);
 globalThis.gl = glCtx;
 
-/*
-    Make sure we have both EXT_color_buffer_float and OES_texture_float_linear extensions in order to have HDR rendering.
-*/
+// Make sure we have both EXT_color_buffer_float and OES_texture_float_linear extensions in order to have HDR rendering.
 if (!gl.getExtension("EXT_color_buffer_float")) 
     throw new EngineError('Engine', 'Extension EXT_color_buffer_float is not supported by your system');
 if (!gl.getExtension("OES_texture_float_linear")) 
     throw new EngineError('Engine', 'Extension OES_texture_float_linear is not supported by your system');
 
+// Now get the 2D canvas context
+const mc2d = $('#timeline-moment-indicator-canvas');
+const mc2dCtx = (mc2d.get(0) as HTMLCanvasElement).getContext('2d');
+if (!mc2dCtx) throw new EngineError('Engine', `Failed to acquire 2D context`);
+globalThis.moment2DCanvas = mc2dCtx;
+
+// Initialize the visualizer, this will trigger all the loading and initialization
 new Visualizer();
 
 canvas.on('mousedown', e => {
