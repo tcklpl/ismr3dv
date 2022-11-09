5889e224161217433d09424393816deb533164b7
tcklpl <negroniluan@gmail.com>
2022-11-09 13:55:47 -0300

    bottom hud options now also remain hidden

--- visualizer/src/scripts/ui/ui_bottom_hud.ts
+++ visualizer/src/scripts/ui/ui_bottom_hud.ts
@@ -5,11 +5,19 @@ export class UIBottomHud implements IUI {
 
     private _currentDate = $('#bottom-hud-current-date');
 
+    private _divCurTime = $('#bottom-bud-cur-time');
+    private _divCurStations = $('#station-count-div');
+
     registerEvents(): void {
 
         visualizer.events.on('moment-changed', (moment: number, ...rest) => {
             this.currentDateLabel = (visualizer.session as ISMRSession).timeline.currentMoments[moment].date.toLocaleString();
-        })
+        });
+
+        visualizer.events.on('session-is-present', (status: boolean, ...rest) => {
+            this._divCurTime.toggleClass('d-inline-block', status).toggleClass('d-none', !status);
+            this._divCurStations.toggleClass('d-inline-block', status).toggleClass('d-none', !status);
+        });
     }
 
     set currentDateLabel(s: string) {
